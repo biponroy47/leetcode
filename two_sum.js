a0e6408c1f4ebcd276fa8d093b6a6decae30ed0f
biponroy47 <biponroy47@yahoo.com>
2022-09-02 16:08:10 -0400

    two sum solution

---
+++ two_sum.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (nums, target) {
+  for (let x = 0; x < nums.length - 1; x++) {
+    for (let y = x + 1; y < nums.length; y++) {
+      if (nums[x] + nums[y] == target) return [x, y];
+    }
+  }
+};
